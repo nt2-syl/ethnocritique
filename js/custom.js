a0ede5465cfdf0bae92a33a5cb8da4d69ddc72e1
Kienan Stewart <kienan@koumbit.org>
2018-11-13 14:58:43 -0500

    Remove empty accordions
    Refs #28188

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,18 @@
-/**/
+(function ($, Drupal) {
+  'use-strict';
+
+  //console.log(document.location);
+  //console.log(Drupal);
+  Drupal.behaviors.ethno_clear_empty_field_group_accordions = {
+    attach: function (context, settings) {
+      $.each($(".field-group-accordion-wrapper"), function(index, element) {
+        //console.log(element);
+        var has_children = $('.ui-accordion-content', element).children();
+        //console.log(has_children);
+        if (has_children.length <= 0) {
+          element.remove();
+        }
+      });
+    }
+  };
+})(jQuery, Drupal);
